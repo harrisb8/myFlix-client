a9cef383fef364b686ea64ae6114c13138b291a2
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-29 21:40:11 -0400

    adding a background color

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -17,6 +17,8 @@ import { GenreView } from '../genre-view/genre-view';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import './main-view.scss';
+
 export class MainView extends React.Component {
 
     constructor(){
